fe5d11cc4371a2c78f9ab5ffaeb18a2fd9830bc0
DenisWritesCode <deniswritescode@gmail.com>
2021-06-09 08:55:28 +0200

    Changed to using object for state

--- src/Components/CVEditor/Creater/CreateWorkForm.js
+++ src/Components/CVEditor/Creater/CreateWorkForm.js
@@ -1,62 +1,71 @@
-import React from 'react'
+import React from "react";
 
-export default function CreateWorkForm({hash, handleDelete}) {
+export default function CreateWorkForm({ data, handleDelete }) {
+  const hash = data.index;
 
+  function handleCheck(element) {
+    const endDate = document.querySelector(`#stopDate${hash}`); // Select stop date
 
-    function handleCheck(element) {
-        const endDate = document.querySelector(`#stopDate${hash}`); // Select stop date
-    
-        if(element.checked){
-          endDate.classList.add("hidden");
-        }
-        else {
-          element.classList.remove("hidden");
-        }
+    if (element.checked) {
+      endDate.classList.add("hidden");
+    } else {
+      endDate.classList.remove("hidden");
     }
+  }
 
-    function handleSubmit(e) {
-      e.preventDefault();
+  function handleSubmit(e) {
+    e.preventDefault();
 
-      console.log(hash, " Submitted");
-      
-    }
+    console.log(hash, " Submitted");
+  }
 
-    // When delete btn is pressed
-    function callHandleDelete(elementHash) {
-      handleDelete(elementHash);
-    }
+  // When delete btn is pressed
+  function callHandleDelete(elementHash) {
+    handleDelete(elementHash);
+  }
 
-    return (
-        <div>
-            <form action="#" onSubmit={handleSubmit}>
-            <label htmlFor="">
-                <p>Job Title: </p>
-                <input type="text" name="jobTitle" id="jobTitle" />
-              </label>
-              <label htmlFor="organisation">
-                <p>Organisation: </p>
-                <input type="text" name="organisation" id="organisation" />
-              </label>
-              <label htmlFor="startDate">
-                <p>Start Date: </p>
-                <input type="month" name="startDate" id="startDate" />
-              </label>
-              <label htmlFor="workHere">
-                <p>Currently work here: </p>
-                <input type="checkbox" name="workHere" id="workHere" onClick={(e) => handleCheck(e.target)} />
-              </label>
-              <label htmlFor="stopDate" id={`stopDate${hash}`}>
-                <p>Stop Date: </p>
-                <input type="month" name="stopDate" id="stopDate" />
-              </label>
-              <label htmlFor="">
-                <p>Job Description: </p>
-                <input type="text" name="" id="" />
-              </label>
-              <button type="submit">Done</button>
-              <button onClick={()=> {callHandleDelete(hash)}}>Delete {hash}</button>
-            </form>
-            <hr />
-        </div>
-    )
+  return (
+    <div>
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="">
+          <p>Job Title: </p>
+          <input type="text" name="jobTitle" id="jobTitle" />
+        </label>
+        <label htmlFor="organisation">
+          <p>Organisation: </p>
+          <input type="text" name="organisation" id="organisation" />
+        </label>
+        <label htmlFor="startDate">
+          <p>Start Date: </p>
+          <input type="month" name="startDate" id="startDate" />
+        </label>
+        <label htmlFor="workHere">
+          <p>Currently work here: </p>
+          <input
+            type="checkbox"
+            name="workHere"
+            id="workHere"
+            onClick={(e) => handleCheck(e.target)}
+          />
+        </label>
+        <label htmlFor="stopDate" id={`stopDate${hash}`}>
+          <p>Stop Date: </p>
+          <input type="month" name="stopDate" id="stopDate" />
+        </label>
+        <label htmlFor="">
+          <p>Job Description: </p>
+          <input type="text" name="" id="" />
+        </label>
+        <button type="submit">Done</button>
+        <button
+          onClick={() => {
+            callHandleDelete(hash);
+          }}
+        >
+          Delete {hash}
+        </button>
+      </form>
+      <hr />
+    </div>
+  );
 }
--- src/Components/CVEditor/EditWork.js
+++ src/Components/CVEditor/EditWork.js
@@ -3,35 +3,58 @@ import { useState } from "react";
 import CreateWorkForm from "./Creater/CreateWorkForm";
 
 function EditWork({ setWork, toggleVisibility }) {
-  const [hashCount, setHashCount] = useState([0]); //Should be an object
+  // const freshState = {
+  //   index: 0,
+  //   title: "",
+  //   organisation: "",
+  //   startDate: "",
+  //   workHere: "",
+  //   stopDate: "",
+  //   jobDescription: "",
+  // };
+  const [hashCount, setHashCount] = useState([]); // Fresh start
 
+  function makeWork(hash) {
+    return {
+      index: hash,
+      title: "",
+      organisation: "",
+      startDate: "",
+      workHere: "",
+      stopDate: "",
+      jobDescription: "",
+    };
+  }
   // Adds count to array
   function handleAddEducation() {
-
-    if(hashCount.length === 0) {
-      // If hashCount is empty, append 0.
-      setHashCount([0]); // An object
+    if (hashCount.length === 0) {
+      // If hashCount is empty, append freshState.
+      const freshState = makeWork(0);
+      setHashCount([{ ...freshState }]);
     } else {
-      const lastElement = hashCount.length - 1;
-      const lastHash = hashCount[lastElement];
-      setHashCount([...hashCount, lastHash + 1]);
+      const lastElement = hashCount.length - 1; // Select our last object
+      const lastHash = hashCount[lastElement].index; // Get the index of selected object
+      const newWorkData = makeWork(lastHash + 1); // Make new object with incremented index
+      setHashCount([...hashCount, newWorkData]); // Change state by appending new work Data
     }
   }
 
   function handleDelete(hash) {
     // filter creates a new array with all enements that pass the test implemented by the provided function.
-    setHashCount((hashArray) => hashArray.filter((element) => hash !== element)); // Delete the selected element.
-    if (hashCount.length < 1) {
-      hashCount.push(0);
-      console.log("Pushed: ",hashCount);
-    }
+    setHashCount((hashArray) =>
+      hashArray.filter((element) => hash !== element.index)
+    ); // Delete the selected element.
   }
 
   return (
     <div className="">
       {hashCount.map((hash) => {
         return (
-          <CreateWorkForm key={hash} hash={hash} handleDelete={handleDelete} />
+          <CreateWorkForm
+            key={hash.index}
+            data={hash}
+            handleDelete={handleDelete}
+          />
         );
       })}
       <button onClick={handleAddEducation}>Add Education</button>
